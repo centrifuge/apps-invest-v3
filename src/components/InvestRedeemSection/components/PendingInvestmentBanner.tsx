a25fa19f1724b24ad6d6ad5f6cc66387afe99400
Jeremiah McCurdy <jeremiah.mccurdy@gmail.com>
2025-09-11 11:13:11 +0200

    feat: add pending investments banner

---
+++ src/components/InvestRedeemSection/components/PendingInvestmentBanner.tsx
@@ -0,0 +1,31 @@
+import { IoMdTimer } from 'react-icons/io'
+import { Balance } from '@centrifuge/sdk'
+import { Box, Flex, Icon, Text } from '@chakra-ui/react'
+import { BalanceDisplay } from '@ui'
+
+interface PendingInvestmentBannerProps {
+  label: string
+  amount: Balance
+  currencySymbol?: string
+}
+
+export function PendingInvestmentBanner({ label, amount, currencySymbol }: PendingInvestmentBannerProps) {
+  return (
+    <Box border="1px solid" borderColor="gray.800" borderRadius="12px" p={4} mb={4}>
+      <Flex alignItems="center" justifyContent="space-between">
+        <Flex alignItems="center" justifyContent="flex-start">
+          <Icon size="sm" mr={2} mt={1}>
+            <IoMdTimer color="gray.400" />
+          </Icon>
+          <Text fontSize="sm">{label}</Text>
+        </Flex>
+        <Flex alignItems="center" justifyContent="flex-end">
+          <BalanceDisplay balance={amount} fontSize="md" fontWeight={600} />
+          <Text fontSize="md" fontWeight={600} ml={2}>
+            {currencySymbol}
+          </Text>
+        </Flex>
+      </Flex>
+    </Box>
+  )
+}
