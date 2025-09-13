c7be0b60c52eda0d6aef421dfa9fa6d0be503bcd
Jeremiah McCurdy <jeremiah.mccurdy@gmail.com>
2025-09-13 10:45:33 +0200

    feat: update heading recipe

--- src/ui/theme/recipes/headingRecipe.ts
+++ src/ui/theme/recipes/headingRecipe.ts
@@ -1,5 +1,4 @@
 import { defineRecipe } from '@chakra-ui/react'
-import { textStylesValues } from 'src/ui/theme/baseStyles/text'
 
 export const headingRecipe = defineRecipe({
   className: 'heading',
@@ -9,8 +8,19 @@ export const headingRecipe = defineRecipe({
   },
   variants: {
     size: {
-      h1: textStylesValues.h1.value,
-      h2: textStylesValues.h2.value,
+      h1: { textStyle: 'h1' },
+      h2: { textStyle: 'h2' },
+      xs: { fontSize: 'xs' },
+      sm: { fontSize: 'sm' },
+      md: { fontSize: 'md' },
+      lg: { fontSize: 'lg' },
+      xl: { fontSize: 'xl' },
+      '2xl': { fontSize: '2xl' },
+      '3xl': { fontSize: '3xl' },
+      '4xl': { fontSize: '4xl' },
+      '5xl': { fontSize: '5xl' },
+      '6xl': { fontSize: '6xl' },
+      '7xl': { fontSize: '7xl' },
     },
   },
   defaultVariants: {
