8470f505ea96063943c75e54530846eaee598d2c
Jeremiah McCurdy <jeremiah.mccurdy@gmail.com>
2025-09-13 10:46:39 +0200

    feat: update theme export

--- src/ui/theme/lightTheme.ts
+++ src/ui/theme/lightTheme.ts
@@ -9,7 +9,7 @@ import { fonts, fontSizes, textStyles } from 'src/ui/theme/baseStyles/text'
 import { textRecipe } from 'src/ui/theme/recipes/textRecipe'
 import { headingRecipe } from 'src/ui/theme/recipes/headingRecipe'
 
-export const lightTheme = defineConfig({
+const lightTheme = defineConfig({
   globalCss: {
     body: {
       color: blackScale[800],
@@ -25,13 +25,10 @@ export const lightTheme = defineConfig({
       fontSizes,
       colors,
     },
-    textStyles,
-    // Read up on semantic tokens here: https://chakra-ui.com/docs/theming/customization/colors#semantic-tokens
-    // When we define default matching tokens for brand colors (i.e. solid, contrast, fg, etc), it allows the use
-    // of the `colorPalette` property on components like buttons and all the colors for all states will be set.
     semanticTokens: {
       colors: colorTokens,
     },
+    textStyles,
     recipes: {
       badge: badgeRecipe,
       button: buttonRecipe,
@@ -45,3 +42,5 @@ export const lightTheme = defineConfig({
     },
   },
 })
+
+export default lightTheme
