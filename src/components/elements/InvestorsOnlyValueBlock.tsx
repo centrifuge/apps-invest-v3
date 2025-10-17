68cba2f37dfb6cfcd019402f4cec3aded8e8ab6c
Jeremiah McCurdy <jeremiah.mccurdy@gmail.com>
2025-10-17 10:21:16 +0200

    refactor: accredited only block

--- src/components/elements/InvestorsOnlyValueBlock.tsx
+++ src/components/elements/InvestorsOnlyValueBlock.tsx
@@ -1,13 +1,13 @@
 import { Box, Flex, Text } from '@chakra-ui/react'
 import { IconInfo } from '@ui'
 
-export function AccreditedOnlyValueBlock() {
+export function InvestorsOnlyValueBlock() {
   return (
     <Box bgColor="gray.300" borderRadius="6px" px={2} py={1}>
       <Flex alignItems="center" justifyContent="flex-start">
         <IconInfo size={16} color="#717680" />
         <Text color="gray.600" fontSize="sm" fontWeight={400} ml={1} strokeWidth="2">
-          Accredited only
+          Investors only
         </Text>
       </Flex>
     </Box>
